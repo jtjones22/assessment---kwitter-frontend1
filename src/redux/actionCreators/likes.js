c52e7e7430fff731ba44f2a0e662eb7d77a9dea6
Janelle Kuhns <j3ckuhns@gmail.com>
2019-12-03 16:29:57 -0800

    started setup for like action creator and reducer

--- src/redux/actionCreators/likes.js
+++ src/redux/actionCreators/likes.js
@@ -1,4 +1,48 @@
-//import { domain, jsonHeaders, handleJsonResponse } from "./constants";
-//import { ADDLIKE, REMOVELIKE } from "../actionTypes";
 
-//const url = domain + "/likes";
+
+import { domain, jsonHeaders, handleJsonResponse } from "./constants";
+import { ADDLIKE, REMOVELIKE } from "../actionTypes";
+
+const url = domain + "/likes";
+
+export const addLike => () => dispatch => {
+  dispatch({
+    type: ADDLIKE.START
+  });
+
+  return fetch(url, {
+    method: "POST",
+    headers: jsonHeaders,
+  })
+    .then(handleJsonResponse)
+    .then(result => {
+      return dispatch({
+        type: ADDLIKE.SUCCESS,
+        payload: result
+      });
+    })
+    .catch(err => {
+      return Promise.reject(dispatch({ type: ADDLIKE.FAIL, payload: err }));
+    });
+};
+
+export const removeLike = username => dispatch => {
+    dispatch({
+      type: REMOVELIKE.START
+    });
+  
+    return fetch(url + "/" + likeId, {
+      method: "DELETE",
+      headers: jsonHeaders,
+    })
+      .then(handleJsonResponse)
+      .then(result => {
+        return dispatch({
+          type: REMOVELIKE.SUCCESS,
+          payload: result
+        });
+      })
+      .catch(err => {
+        return Promise.reject(dispatch({ type: REMOVELIKE.FAIL, payload: err }));
+      });
+  };
