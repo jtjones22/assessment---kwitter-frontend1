babe017f948273f28d43a2bc4002219f5ed153d4
Jonathan Jones <jonathan.jones51@yahoo.com>
2019-12-05 11:51:44 -0500

    Added the ability to confirm or deny deleting a user or message

--- src/react/components/DeleteUser.js
+++ src/react/components/DeleteUser.js
@@ -3,6 +3,15 @@ import "semantic-ui-css/semantic.min.css";
 import { withAsyncAction, connect } from "../HOCs";
 
 class DeleteUser extends Component {
+
+    handleDeleteUser = () => {
+        const confrim = window.confirm()
+
+        if(confrim) {
+            this.props.deleteUser(this.props.id)
+        }
+    }
+
   render() {
     return (
       this.props.username === this.props.loggedInUser && (
@@ -11,7 +20,7 @@ class DeleteUser extends Component {
             background: "red"
           }}
           onClick={() => {
-            this.props.deleteUser(this.props.username);
+            this.handleDeleteUser();
           }}
         >
           Delete Account
