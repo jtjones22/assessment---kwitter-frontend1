e54db95429b2f66ea91990f023320c544ee24195
Slukas <sashamati@gmail.com>
2019-12-05 14:40:06 -0800

    profile user pic

--- src/react/components/ProfileCard.js
+++ src/react/components/ProfileCard.js
@@ -5,11 +5,20 @@ import { withAsyncAction } from "../HOCs"
 import { Spinner } from '../components';
 import DeleteUser from './DeleteUser'
 
-class ProfileCard extends Component {
 
-  componentDidMount() {
-    this.props.getUser(this.props.username)
-  }
+
+  class ProfileCard extends React.Component {
+    componentDidMount() {
+      this.props.getUser(this.props.username);
+    }
+  
+    componentDidUpdate(prevProps) {
+      // Typical usage (don't forget to compare props):
+      if (this.props.username !== prevProps.username) {
+        this.props.getUser(this.props.username);
+      }
+    }
+  
 
   render() {
     if(this.props.result === null) {
@@ -21,7 +30,8 @@ class ProfileCard extends Component {
     return (
       <Card>
         <Image
-          src={user.pictureLocation}
+          src={user.pictureLocation 
+          ? "https://kwitter-api.herokuapp.com" + user.pictureLocation : "https://icon-library.net/images/no-profile-picture-icon-female/no-profile-picture-icon-female-9.jpg" }
           wrapped
           ui={false}
         />
@@ -62,6 +72,7 @@ class ProfileCard extends Component {
     );
   }
 }
+export default withAsyncAction("users", "getUser")(ProfileCard);
 
 /*mapStateToProps
   loading
@@ -72,4 +83,3 @@ mapDispatchToProps
   getUser
 */
 
-export default withAsyncAction("users","getUser")(ProfileCard);
