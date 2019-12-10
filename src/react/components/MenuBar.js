1d00aeb7b05f0b2f50e38fbc25ee73c3cf1ee54b
Janelle Kuhns <j3ckuhns@gmail.com>
2019-12-10 13:16:46 -0800

    updated menubar with ui

---
+++ src/react/components/MenuBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from ".";
+import "./Menu.css";
+import { withAsyncAction } from "../HOCs";
+import { Menu, MenuItem } from "semantic-ui-react";
+
+class MenuBar extends React.Component {
+  handleLogout = event => {
+    event.preventDefault();
+    this.props.logout();
+  };
+
+  render() {
+    return (
+      <div id="menu">
+        <Link to="/">
+        <h1 style={{
+          color: "black"
+        }}>Kwitter</h1>
+        </Link>
+        {this.props.isAuthenticated && (
+          <Menu id="menu-links">
+            <MenuItem as={Link} to="/messagefeed" name="Message Feed"></MenuItem>
+            <MenuItem as={Link} to="/" name="Profile"></MenuItem>
+            <MenuItem as={Link} to="/" onClick={this.handleLogout} name="Logout"></MenuItem>
+          </Menu>
+        )}
+      </div>
+    );
+  }
+}
+
+export default withAsyncAction("auth", "logout")(MenuBar)
--- src/react/pages/MessageFeed.js
+++ src/react/pages/MessageFeed.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { CreateMessageForm, Menu, GlobalMessageList } from "../components";
+import { CreateMessageForm, GlobalMessageList, MenuBar } from "../components";
 import { userIsAuthenticated } from "../HOCs";
 
 class MessageFeed extends React.Component {
@@ -7,7 +7,7 @@ class MessageFeed extends React.Component {
     render() {
         return (
         <>
-        <Menu isAuthenticated={this.props.isAuthenticated} />
+        <MenuBar isAuthenticated={this.props.isAuthenticated} />
         <h2>Message Feed</h2>
         <CreateMessageForm />
         <GlobalMessageList />
