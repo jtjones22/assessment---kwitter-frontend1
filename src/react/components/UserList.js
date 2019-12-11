492b77e29992d8fbc42e77c6559aa8b7917ff9f2
Jonathan Jones <jonathan.jones51@yahoo.com>
2019-12-11 13:52:16 -0500

    Restructured the search page

--- src/react/components/UserList.js
+++ src/react/components/UserList.js
@@ -4,31 +4,31 @@ import UserCard from "./UserCard";
 import { Link } from ".";
 
 class UserList extends Component {
-    state = {
-        filteredUsers: []
-    }
+  state = {
+    filteredUsers: []
+  };
 
   componentDidMount() {
     this.props.getUsers();
   }
 
   componentDidUpdate(prevProp) {
-      if (prevProp.result !== this.props.result) {
-          this.setState({filteredUsers: this.props.result.users})
-      }
+    if (prevProp.result !== this.props.result) {
+      this.setState({ filteredUsers: this.props.result.users });
+    }
   }
 
   handleSearchUser = event => {
-      const filterValues = event.target.value
-      const newFilteredUsers = this.props.result.users.filter(user => {
-          if(user.username.includes(filterValues)) {
-              return true
-          } else {
-              return false
-          }
-        })
-        this.setState({filteredUsers: newFilteredUsers})
-  }
+    const filterValues = event.target.value;
+    const newFilteredUsers = this.props.result.users.filter(user => {
+      if (user.username.includes(filterValues)) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    this.setState({ filteredUsers: newFilteredUsers });
+  };
 
   render() {
     if (this.props.result === null) {
@@ -36,31 +36,47 @@ class UserList extends Component {
     }
     return (
       <>
-        <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
           <input
             style={{
-              width: "100%"
+              width: "45%",
+              height: "30px"
             }}
             onChange={this.handleSearchUser}
             placeholder="Enter a username"
           ></input>
         </div>
-        {this.props.result && this.state.filteredUsers.map(user => (
-          <div
-            style={{
-              marginBottom: "15px"
-            }}
-          >
-            <Link to={`/profile/${user.username}`}>
-              <UserCard
-                username={user.username}
-                bio={user.about}
-                profilePicture={user.pictureLocation}
-                displayName={user.displayName}
-              />
-            </Link>
-          </div>
-        ))}
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "auto auto auto",
+            justifyItems: "center",
+            marginBottom: "10px"
+          }}
+        >
+          {this.props.result &&
+            this.state.filteredUsers.map(user => (
+              <div
+                style={{
+                  marginBottom: "15px"
+                }}
+              >
+                <Link to={`/profile/${user.username}`}>
+                  <UserCard
+                    username={user.username}
+                    bio={user.about}
+                    profilePicture={user.pictureLocation}
+                    displayName={user.displayName}
+                  />
+                </Link>
+              </div>
+            ))}
+        </div>
       </>
     );
   }
